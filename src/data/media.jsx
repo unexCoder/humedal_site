80201cb403b0590f2d7da046ebf654c8048fc5e9
Luigi Tamagnini <bosca.music@gmail.com>
2023-05-31 09:09:26 -0300

    new pics

--- src/data/media.jsx
+++ src/data/media.jsx
@@ -4,9 +4,18 @@ const gallery = [
     {key:2, src: 'https://live.staticflickr.com/65535/52935231903_01c3c040df_o.jpg',caption:'lectures'},
     {key:3, src: 'https://live.staticflickr.com/65535/52935176750_207fb94e17_o.jpg',caption:'sonicart'},
     {key:4, src: 'https://live.staticflickr.com/65535/52935182950_9e8b9064a0_o.jpg',caption:'concerts'},
-    {key:5, src: 'https://live.staticflickr.com/65535/52934215427_54c33b1e82_o.jpg',caption:'lectures'},
+    {key:6, src: 'https://live.staticflickr.com/65535/52940372043_8e4816fbcc_o.jpg',caption:'workshops'},
+    {key:5, src: 'https://live.staticflickr.com/65535/52939320312_695a449d81_o.jpg',caption:'sonicart'},
+    {key:7, src: 'https://live.staticflickr.com/65535/52940303725_721decf07d_o.jpg',caption:'concerts'},
+    {key:8, src: 'https://live.staticflickr.com/65535/52940060749_fae814d407_o.jpg',caption:'workshops'},
 ];
 
 export default gallery;
+
+
+
+
+
+
 
 
